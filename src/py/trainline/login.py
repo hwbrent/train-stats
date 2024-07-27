1c75dacfe977fed607f95a48047f2e843162dfee
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-07-27 20:57:59 +0100

    Get webdriver

--- src/py/trainline/login.py
+++ src/py/trainline/login.py
@@ -1,8 +1,15 @@
-from ..chromedriver_utils.init import init
+import os
+
+from chromedriver_utils.init import init
+
+
+PARENT_DIR = os.path.dirname(__file__)
+PY_ROOT = os.path.join(PARENT_DIR, os.pardir)
+CHROMEDRIVER_PATH = os.path.join(PY_ROOT, "chromedriver")
 
 
 def login():
-    pass
+    driver = init(CHROMEDRIVER_PATH)
 
 
 if __name__ == "__main__":
