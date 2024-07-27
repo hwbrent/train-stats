01b5c7b129d83de20f29ccbe274d8ca70859362c
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-07-27 21:07:44 +0100

    Fill in fields and click sign-in button

--- src/py/trainline/login.py
+++ src/py/trainline/login.py
@@ -1,5 +1,7 @@
 import os
 
+from selenium.webdriver.common.by import By
+
 from chromedriver_utils.init import init
 
 
@@ -8,8 +10,27 @@ PY_ROOT = os.path.join(PARENT_DIR, os.pardir)
 CHROMEDRIVER_PATH = os.path.join(PY_ROOT, "chromedriver")
 
 
+LOGIN_PAGE_URL = "https://www.thetrainline.com/en-us/my-account/login"
+
+
 def login():
+    # Get webdriver
     driver = init(CHROMEDRIVER_PATH)
+
+    # Navigate to login page
+    driver.get(LOGIN_PAGE_URL)
+
+    # Find email field and fill it in
+    email_input = driver.find_element(By.ID, "signin-email")
+    email_input.send_keys()
+
+    # Find password field and fill it in
+    password_input = driver.find_element(By.ID, "signin-password")
+    password_input.send_keys()
+
+    # Find submit button and click it
+    signin_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    signin_button.click()
 
 
 if __name__ == "__main__":
